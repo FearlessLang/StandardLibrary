72ea113ea4221f5949babcf698de4772dd5f7d3f
Marco Servetto <marco.servetto@gmail.com>
2026-02-16 21:45:03 +1300

    empty infos

--- rt/_Throw$0.java
+++ rt/_Throw$0.java
@@ -13,15 +13,6 @@ public interface _Throw$0{
     var info$= (Info$0)p0;
     throw Util.nonDeterministic(info$);//throw new Error(info$.read$str$0().toString());
   }
-  /*default Object imm$stackTrace$0(){
-    var this$= this;
-    var al=new ArrayList<>();
-    al.add(new Str$0Instance("AAA"));
-    al.add(new Str$0Instance("BBB"));
-    var list= new List$1Instance(al);
-    var dtId=new DataTypeBy$3(){ @Override public Object imm$$hash$1(Object p0){ return p0; } };
-    return list.read$info$1(dtId);
-  }*/
   default Object imm$stackTrace$0(){
     var st= new Throwable().getStackTrace();
     var al= new ArrayList<>();
